b57468489d0badfce7a4cd67cd0acd8dac87097b
Aarya Ghate <136184830+AaryaGhate@users.noreply.github.com>
2023-11-01 08:46:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import random
 # Load data
 data = pd.read_csv("newdata.csv")
 
-# Function to get book recommendations based on genre and author
-def get_recommendations(book_name, author, interaction_matrix, num_recommendations=2):
-    filtered_books = data[((data['genre'] == genre) || (data['author'] == author)) & (data['book name'] != book_name)]
+# Function to get popular books in the same genre
+def get_recommendations(book_name, genre, num_recommendations=2):
+    filtered_books = data[(data['genre'] == genre) & (data['book name'] != book_name)]
     if filtered_books.empty:
         return None
     
-    random_recommendations = filtered_books.sample(min(num_recommendations, len(filtered_books)))
-    return random_recommendations[['book id', 'book name', 'author', 'genre', 'Price', 'Rating', 'publication', 'number of pages']]
+    popular_recommendations = filtered_books.nlargest(num_recommendations, 'Rating')[['book id', 'book name', 'author', 'genre', 'Price', 'Rating', 'publication', 'number of pages']]
+    return popular_recommendations
 
 # Streamlit app
 def main():
@@ -27,11 +27,11 @@ def main():
     
     # User input
     book_name = st.selectbox("Select Book Name", data['book name'].unique())
-    author = st.selectbox("Select Author", data['author'].unique())
+    genre = st.selectbox("Select Genre", data['genre'].unique())
    
     # Recommendation button
     if st.button("Get Recommendations"):
-        recommendations = get_recommendations(book_name, author, data)
+        recommendations = get_recommendations(book_name, genre)
         
         if recommendations is not None and not recommendations.empty:
             st.subheader("Recommended Books:")
