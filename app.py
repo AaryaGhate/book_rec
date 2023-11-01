66eef0049e20c1147f94bec2f4590e0cc059adb8
Aarya Ghate <136184830+AaryaGhate@users.noreply.github.com>
2023-11-01 08:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,26 @@ data = pd.read_csv("newdata.csv")
 interaction_matrix = data.pivot_table(index='User ID', columns='book id', values='Rating', fill_value=0)
 product_similarity = cosine_similarity(interaction_matrix.T)
 
-# Function to get book recommendations based on book name and author
-def get_recommendations(user_id, book_name, author, interaction_matrix, product_similarity, num_recommendations=2):
+# Function to get book recommendations based on book name, author or genre
+def get_recommendations(user_id, book_name, attribute, interaction_matrix, product_similarity, num_recommendations=2):
     if user_id not in interaction_matrix.index:
         return None
     
     user_interactions = interaction_matrix.loc[user_id].values
-    similar_scores = product_similarity.dot(user_interactions)
+    
+    if attribute == 'author':
+        attribute_values = data['author']
+    elif attribute == 'genre':
+        attribute_values = data['genre']
+    else:
+        raise ValueError("Invalid attribute. Use 'author' or 'genre'.")
+    
+    attribute_indices = attribute_values[attribute_values == book_name].index
+    similar_scores = product_similarity[attribute_indices].mean(axis=0)
     recommended_indices = similar_scores.argsort()[-num_recommendations:][::-1]
     recommended_books = interaction_matrix.columns[recommended_indices]
     
-    # Filter recommended books by book name and author
-    filtered_books = filter_by_book_name_and_author(recommended_books, book_name, author)
-    
-    return filtered_books
-
-# Function to filter recommended books by book name and author
-def filter_by_book_name_and_author(books, book_name, author):
-    filtered_books = data[data['book name'] == book_name]
-    filtered_books = filtered_books[filtered_books['author'] == author]
-    return filtered_books['book id']
+    return recommended_books
 
 # Streamlit app
 def main():
@@ -45,11 +45,11 @@ def main():
     # User input
     user_id = st.number_input("Enter User ID", min_value=1, max_value=1000)
     book_name = st.selectbox("Select Book Name", data['book name'].unique())
-    author = st.selectbox("Select Author", data['author'].unique())
+    attribute_type = st.radio("Select Attribute for Similarity", ('author', 'genre'))
    
     # Recommendation button
     if st.button("Get Recommendations"):
-        recommendations = get_recommendations(user_id, book_name, author, interaction_matrix, product_similarity)
+        recommendations = get_recommendations(user_id, book_name, attribute_type, interaction_matrix, product_similarity)
         
         if recommendations is not None and len(recommendations) > 0:
             random_recommendations = random.sample(list(recommendations), min(2, len(recommendations)))
